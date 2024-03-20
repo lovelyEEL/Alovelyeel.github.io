9c4d519f7f1f8a72ce5cfdf514cd0f9b6c49519f
17520332237 <1260754882@qq.com>
2024-03-20 22:02:06 +0800

    mmrr

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
 // export default defineConfig({
 //   publicPath: "/mmrr/"
 // })
-
-import { defineConfig } from '@vue/cli-service'
-export default defineConfig({
-  transpileDependencies: true,
-  publicPath: '/mmrr/',    //gitee远程库名
-  outputDir: 'dist/',
-  assetsDir: 'static/'
-})
+const BASE_URL = process.env.NODE_ENV === "production" ? "/mmrr/" : "./";
+export const publicPath = BASE_URL;
+// import { defineConfig } from '@vue/cli-service'
+// export default defineConfig({
+//   transpileDependencies: true,
+//   publicPath: '/mmrr/',    //gitee远程库名
+//   outputDir: 'dist/',
+//   assetsDir: 'static/'
+// })
