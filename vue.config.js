2f4c239e950f2d9fb88fcd48e712267255f11c9b
17520332237 <1260754882@qq.com>
2024-03-20 18:02:35 +0800

    mmrr

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/mmrr/',    //gitee远程库名
-  outputDir: 'dist/',
-  assetsDir: 'static/'
+  publicPath: "/mmrr/",
+  base: './',
 })
