1dfa0bf8264d2ef6ee4b44267380437c48e7d642
17520332237 <1260754882@qq.com>
2024-03-20 16:58:32 +0800

    mr

--- mmrr/vue.config.js
+++ mmrr/vue.config.js
@@ -1,7 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/mmrr/',    //gitee远程库名
-  outputDir: 'dist/',
-  assetsDir: 'static/'
-})
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/mmrr'
+        : '/'
+    ,
+    productionSourceMap: false,
+}
